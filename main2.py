9eede6d923c2020f029ecd6bec5cd23cece0f2bc
sapphira23 <125115366+sapphira23@users.noreply.github.com>
2023-05-24 15:08:01 +0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,28 @@
+import requests
+
+s_city = "Moscow,RU"
+
+appid = "Your token"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+
+data = res.json()
+
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+print("Скорость ветра:", data['wind']['speed'])
+print("Видимость:", data['visibility'])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата<", i['dt_txt'], ">\r\nТемпературa<",
+          '{0:3.0f}'.format(i['main']['temp']), ">\r\nПогодные условия<", i['weather'][0]['description'],
+          ">\r\nСкорость ветра<", i['wind']['speed'],
+          ">\r\nВидимость<", i['visibility'], ">")
